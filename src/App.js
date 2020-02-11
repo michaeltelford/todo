3757883318057ae365e76bdfb277fe688685186e
Michael Telford <michael.telford@live.com>
2020-02-11 18:14:12 +0000

    Added todo state modifier logic

--- src/App.js
+++ src/App.js
@@ -1,27 +1,70 @@
 import React from 'react';
 import './App.css';
-import Small from './Small';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+
     this.state = {
-      tool: 'React',
+      todos: [
+        {
+          name: 'Add some TODOs',
+          done: false,
+        },
+      ],
     };
   }
 
   render() {
     return (
       <>
-        <Small tool={this.state.tool} callback={this.updateTool} />
-        <p>The parent's state is: {this.state.tool}</p>
+        <input />
+        {this.state.todos.map(todo => {
+          return (
+            <>
+              <p onClick={this.handler}>{todo.name} - {todo.done ? 'true' : 'false'}</p>
+              <hr />
+            </>
+          )
+        })}
       </>
     )
   }
 
-  updateTool = (newTool) => {
-    this.setState({
-      tool: newTool,
+  handler = evt => {
+    const text = evt.target.innerText;
+    const value = text.includes('true') ? false : true;
+    this.toggleTodo({ name: 'blah', done: value });
+  }
+
+  /* State Modifiers */
+
+  addTodo = newTodo => {
+    this.setState(prevState => prevState.todos.push(newTodo));
+  }
+
+  removeTodo = obsoleteTodo => {
+    this.setState(prevState => {
+      prevState.todos = prevState.todos.filter(todo => {
+        return todo.name !== obsoleteTodo.name;
+      });
+      return prevState.todos;
+    });
+  }
+
+  toggleTodo = updatedTodo => {
+    this.setState(prevState => {
+      const index = prevState.todos.findIndex(todo => {
+        return todo.name === updatedTodo.name;
+      });
+      prevState.todos.splice(index, 1, updatedTodo);
+      return prevState.todos;
+    });
+  }
+
+  filterTodos = done => {
+    return this.state.todos.filter(todo => {
+      return done ? todo.done : !todo.done;
     });
   }
 }
