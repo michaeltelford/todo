5d630c9ce2453312ba34ededfd8d776b0fff25f1
Michael Telford <michael.telford@live.com>
2020-04-26 12:28:27 +0100

    Added tailwind via purgecss deps

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Router from './components/Router';
 import * as serviceWorker from './serviceWorker';
+import './tailwind.generated.css';
 
 ReactDOM.render(<Router />, document.getElementById('root'));
 
