cd2cfef8e4779339f6e8556ec39d198b2fa90e94
Michael Telford <michael.telford@live.com>
2020-04-26 11:41:22 +0100

    Removed old CSS

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Router from './components/Router';
 import * as serviceWorker from './serviceWorker';
-import './index.css';
 
 ReactDOM.render(<Router />, document.getElementById('root'));
 
