cc5aec279038bbe0c5184f4847c2982796050103
Michael Telford <michael.telford@live.com>
2020-07-11 18:34:06 +0100

    Centered everything at >= 640 px

--- src/components/Wrapper.js
+++ src/components/Wrapper.js
@@ -4,7 +4,7 @@ import { handleLogout } from './Auth';
 
 function Wrapper(props) {
   return (
-    <div className='p-3' style={{minWidth: 230}}>
+    <div className='p-3 ml-auto mr-auto' style={{ minWidth: 230, maxWidth: 640 }}>
       <h1 className='my-4 text-center text-3xl font-light tracking-widest'>
         <Link to='/lists'>
           TODO Checklist
