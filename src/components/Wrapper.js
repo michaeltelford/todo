3094874c0efcbd075638a7493ccfe4402139c80d
Michael Telford <michael.telford@live.com>
2020-08-08 22:30:23 +0100

    Updated <h1> to be responsive

--- src/components/Wrapper.js
+++ src/components/Wrapper.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 function Wrapper(props) {
   return (
     <div style={{ minWidth: 230 }} className='p-3'>
-      <h1 className='my-4 text-center text-3xl font-light tracking-widest'>
+      <h1 className='text-center font-light tracking-widest text-3xl my-8 sm:text-4xl sm:my-10 lg:my-12 xl:text-5xl'>
         <Link to='/lists'>
           TODO Checklist
         </Link>
