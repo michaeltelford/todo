67e39ab130bfda93f43833a2fbdc8058c5e2dc64
Michael Telford <michael.telford@live.com>
2021-06-26 23:11:41 +0100

    Added a top level ErrorBoundary component

--- src/components/Router.js
+++ src/components/Router.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import { AppContext } from '../context';
 import API from '../api';
+import ErrorBoundary from './ErrorBoundary';
 import Auth from './Auth';
 import Wrapper from './Wrapper';
 import Lists from './lists/Lists';
@@ -17,31 +18,33 @@ const api = new API(apiUrl);
 function Router() {
   return (
     <AppContext.Provider value={{ api }}>
-      <BrowserRouter>
-        <Switch>
-          <Route exact path='/list/:id'>
-            <Wrapper>
-              <List />
-            </Wrapper>
-          </Route>
-          <Route exact path='/lists'>
-            <Wrapper>
-              <Lists />
-            </Wrapper>
-          </Route>
-          <Route exact path='/auth'>
-            <Auth />
-          </Route>
-          <Route exact path='/'>
-            <Redirect to='/lists' />
-          </Route>
-          <Route path='*'>
-            <Wrapper>
-              <p>Page not found. Click <a href='/'>here</a> to return to the home page.</p>
-            </Wrapper>
-          </Route>
-        </Switch>
-      </BrowserRouter>
+      <ErrorBoundary>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path='/list/:id'>
+              <Wrapper>
+                <List />
+              </Wrapper>
+            </Route>
+            <Route exact path='/lists'>
+              <Wrapper>
+                <Lists />
+              </Wrapper>
+            </Route>
+            <Route exact path='/auth'>
+              <Auth />
+            </Route>
+            <Route exact path='/'>
+              <Redirect to='/lists' />
+            </Route>
+            <Route path='*'>
+              <Wrapper>
+                <p>Page not found. Click <a href='/'>here</a> to return to the home page.</p>
+              </Wrapper>
+            </Route>
+          </Switch>
+        </BrowserRouter>
+      </ErrorBoundary>
     </AppContext.Provider>
   );
 }
